45534d9e28cfac9a5b5f3cd5e4e43317d9be81e8
Lucas <ima_lucas@hotmail.com>
2020-11-05 11:33:57 +1100

    Added PatrolNode.cs and prefab
    This is a script that currently visually shows connections between nodes. But this may be able to be used later for more specifc node purposes, such as using a node as a trigger for example.
    
    Each node stores a reference to an array of nodes that it is connected to. This will be able to be used as a direction to look for enemy search behaviours

---
+++ Assets/Scripts/Enemy/PatrolNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolNode : MonoBehaviour
+{
+    public Transform[] connections;
+
+    public bool showConnections = false;
+
+    public void DebugDrawConnections(Color colour)
+    {
+        foreach(Transform otherNode in connections)
+        {
+            Debug.DrawLine(transform.position, otherNode.transform.position, colour);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.4f);
+        Gizmos.DrawSphere(transform.position, 0.5f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+
+        if(showConnections)
+        {
+            DebugDrawConnections(Color.green);
+        }
+    }
+}
