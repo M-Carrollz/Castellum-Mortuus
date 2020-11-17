c4d9baba2b8357c0e15e1b07094f729cacaa5dd4
Lucas <ima_lucas@hotmail.com>
2020-11-18 01:05:56 +1100

    Added AudioMixer
    This enables control over volumes of sounds.

---
+++ Assets/Scripts/Audio/SoundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public void PlaySound(AudioSource audio)
+    {
+        audio.Play();
+    }
+
+    public void PlaySoundOneShot(AudioSource audio)
+    {
+        audio.PlayOneShot(audio.clip);
+    }
+
+    public void StopSound(AudioSource audio)
+    {
+        audio.Stop();
+    }
+}
---
+++ Assets/Scripts/Audio/VolumeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public string groupName = "MasterVol";
+
+    public void SetVolume(float sliderValue)
+    {
+        mixer.SetFloat(groupName, Mathf.Log10(sliderValue) * 20);
+    }
+}
